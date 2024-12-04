bbb8421c63ff4413a49180b68fbee6f372b0c4f0
Derek <sjukledighet@gmail.com>
2024-12-04 00:52:35 -0500

    Day 3 final solution

--- p03.py
+++ p03.py
@@ -1,28 +1,23 @@
-from aocd import data, submit
-
 import re
 
-# data = '''xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'''
-# data = '''xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'''
+from aocd import data
 
 p = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)")
 
-# x = 0
-# for a, b in p.findall(data):
-#     x += int(a) * int(b)
-# print(p.findall(data))
-# submit(sum(p.findall()))
-# submit(x)
-
-x = 0
-DO = True
+result = 0
+more_accurate_result = 0
+enabled = True
 for a, b, do, dont in p.findall(data):
     if do:
-        DO = True
+        enabled = True
     elif dont:
-        DO = False
+        enabled = False
 
-    if DO and a.isnumeric() and b.isnumeric():
-        x += int(a) * int(b)
+    if a and b:
+        product = int(a) * int(b)
+        result += product
+        if enabled:
+            more_accurate_result += product
 
-submit(x)
+print("Part 1:", result)
+print("Part 2:", more_accurate_result)
