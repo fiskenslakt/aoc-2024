2f7c3282b31a8f746a2bfdf545eeb628f0f21b51
Derek <sjukledighet@gmail.com>
2024-12-04 03:09:17 -0500

    Day 4 final solution

--- p04.py
+++ p04.py
@@ -1,80 +1,31 @@
-from aocd import data, submit
+from aocd import data
 
-# data = '''MMMSXXMASM
-# MSAMXMSMSA
-# AMXSXMAAMM
-# MSAMASMSMX
-# XMASAMXAMM
-# XXAMMXXAMA
-# SMSMSASXSS
-# SAXAMASAAA
-# MAMMMXMMMM
-# MXMXAXMASX'''
+crossword = {(x, y): c
+             for y, row in enumerate(data.splitlines())
+             for x, c in enumerate(row)}
 
-crossword = data.splitlines()
+max_x, max_y = max(crossword)
+
 xmas = 0
-# import pudb;pu.db
-for y, line in enumerate(crossword):
-    for x, char in enumerate(line):
-        if char != "X":
-            continue
-        for i, j in ((1,0), (0,1), (-1,0), (0,-1), (1,1), (-1,-1), (1,-1), (-1,1)):
-            nx = x
-            ny = y
-            for c in 'MAS':
-                nx = nx + i
-                ny = ny + j
+x_mas = 0
+for y in range(max_y + 1):
+    for x in range(max_x + 1):
+        if crossword.get((x, y)) == "X":
+            for i in (-1, 0, 1):
+                for j in (-1, 0, 1):
+                    nx = x
+                    ny = y
+                    for c in "MAS":
+                        nx += i
+                        ny += j
+                        if crossword.get((nx, ny)) != c:
+                            break
+                    else:
+                        xmas += 1
+        elif crossword.get((x, y)) == "A":
+            if ("".join(crossword.get((x+i, y+i), "") for i in (-1, 1)) in ("SM", "MS")
+                    and "".join(crossword.get((x-i, y+i), "") for i in (-1, 1)) in ("SM", "MS")):
+                x_mas += 1
 
-                if (0 <= nx < len(line)
-                        and 0 <= ny < len(crossword)):
-                    if crossword[ny][nx] != c:
-                        break
-                else:
-                    break
-            else:
-                xmas += 1
-
-# submit(xmas)
-mas = 0
-for y, line in enumerate(crossword):
-    for x, char in enumerate(line):
-        # if x == 140 and y == 5:
-        #     import pudb;pu.db
-        if char != "A":
-            continue
-        if x == 0 or x == len(line) - 1 or y == 0 or y == len(crossword) - 1:
-            continue
-
-        x1, y1 = x-1, y-1
-        x2, y2 = x+1, y-1
-        x3, y3 = x-1, y+1
-        x4, y4 = x+1, y+1
-
-        # print(x2, y2)
-        # print(len(line), len(crossword))
-
-        if (crossword[y1][x1] == "M"
-            and crossword[y2][x2] == "M"
-            and crossword[y3][x3] == "S"
-            and crossword[y4][x4] == "S"):
-            mas += 1
-
-        elif (crossword[y1][x1] == "S"
-            and crossword[y2][x2] == "M"
-            and crossword[y3][x3] == "S"
-            and crossword[y4][x4] == "M"):
-            mas += 1
-
-        elif (crossword[y1][x1] == "S"
-            and crossword[y2][x2] == "S"
-            and crossword[y3][x3] == "M"
-            and crossword[y4][x4] == "M"):
-            mas += 1
-
-        elif (crossword[y1][x1] == "M"
-            and crossword[y2][x2] == "S"
-            and crossword[y3][x3] == "M"
-            and crossword[y4][x4] == "S"):
-            mas += 1
-
-submit(mas)
+print("Part 1:", xmas)
+print("Part 2:", x_mas)
