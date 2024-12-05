7afa8b6c08eeef64a8a91b11825cde01d05320a1
Derek <sjukledighet@gmail.com>
2024-12-05 01:08:28 -0500

    Day 5 final solution

--- p05.py
+++ p05.py
@@ -1,39 +1,30 @@
 from collections import defaultdict
 
-from aocd import data, submit
+from aocd import data
 
-r, u = data.split("\n\n")
+raw_rules, raw_updates = data.split("\n\n")
 
 rules = defaultdict(list)
+for rule in raw_rules.splitlines():
+    page_a, page_b = rule.split("|")
+    rules[page_b].append(page_a)
 
-for rule in r.splitlines():
-    a, b = rule.split("|")
-    rules[b].append(a)
+middle_pages_sum = 0
+unsorted_middle_pages_sum = 0
 
-t = 0
-unsorted = []
-
-for update in u.splitlines():
+for update in raw_updates.splitlines():
+    correctly_ordered = True
     pages = update.split(",")
     for i in range(len(pages)-1):
         for j in range(i + 1, len(pages)):
             if pages[j] in rules[pages[i]]:
-                unsorted.append(pages)
-                break
-        else:
-            continue
-        break
-    else:
-        t += int(pages[len(pages)//2])
-
-# submit(t)
-t2 = 0
-for pages in unsorted:
-    for i in range(len(pages)-1):
-        for j in range(i + 1, len(pages)):
-            if pages[j] in rules[pages[i]]:
+                correctly_ordered = False
                 pages[i], pages[j] = pages[j], pages[i]
 
-    t2 += int(pages[len(pages)//2])
+    if correctly_ordered:
+        middle_pages_sum += int(pages[len(pages)//2])
+    else:
+        unsorted_middle_pages_sum += int(pages[len(pages)//2])
 
-submit(t2)
+print("Part 1:", middle_pages_sum)
+print("Part 2:", unsorted_middle_pages_sum)
