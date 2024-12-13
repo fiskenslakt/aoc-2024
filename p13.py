bde1cc504377688943544ef7b0a07ebf020bf467
Derek <sjukledighet@gmail.com>
2024-12-13 02:22:35 -0500

    Day 13 final solution

--- p13.py
+++ p13.py
@@ -1,61 +1,58 @@
 import re
-from functools import cache
 
-from aocd import data, submit
+from aocd import data
 
 # tokens needed to press
 A_BUTTON_PRICE = 3
 B_BUTTON_PRICE = 1
 
-
-
-# data = """Button A: X+94, Y+34
-# Button B: X+22, Y+67
-# Prize: X=8400, Y=5400
-
-# Button A: X+26, Y+66
-# Button B: X+67, Y+21
-# Prize: X=12748, Y=12176
-
-# Button A: X+17, Y+86
-# Button B: X+84, Y+37
-# Prize: X=7870, Y=6450
-
-# Button A: X+69, Y+23
-# Button B: X+27, Y+71
-# Prize: X=18641, Y=10279"""
-
-
-@cache
-def dfs(ax, ay, bx, by, px, py, tokens, x, y, a_presses, b_presses):
-    if x == px and y == py:
-        return tokens
-    elif x > px or y > py:
-        return float("inf")
-
-    a_press = dfs(ax, ay, bx, by, px, py, tokens + A_BUTTON_PRICE, x + ax, y + ay, a_presses + 1, b_presses)
-    b_press = dfs(ax, ay, bx, by, px, py, tokens + B_BUTTON_PRICE, x + bx, y + by, a_presses, b_presses + 1)
-
-    return min(a_press, b_press)
-
+OFFSET = 10_000_000_000_000
 
 total_tokens = 0
-# import pudb;pu.db
+total_tokens_with_offset = 0
+
 for claw_machine in data.split("\n\n"):
-    machine = {}
-    machine["A"] = re.search(r"Button A: X\+(\d+), Y\+(\d+)", claw_machine).groups()
-    machine["B"] = re.search(r"Button B: X\+(\d+), Y\+(\d+)", claw_machine).groups()
-    machine["prize"] = re.search(r"Prize: X=(\d+), Y=(\d+)", claw_machine).groups()
+    button_a = re.search(r"Button A: X\+(\d+), Y\+(\d+)", claw_machine)
+    button_b = re.search(r"Button B: X\+(\d+), Y\+(\d+)", claw_machine)
+    prize = re.search(r"Prize: X=(\d+), Y=(\d+)", claw_machine)
 
-    ax = int(machine["A"][0])
-    ay = int(machine["A"][1])
-    bx = int(machine["B"][0])
-    by = int(machine["B"][1])
-    px = int(machine["prize"][0])
-    py = int(machine["prize"][1])
+    ax = int(button_a[1])
+    ay = int(button_a[2])
+    bx = int(button_b[1])
+    by = int(button_b[2])
 
-    tokens = dfs(ax, ay, bx, by, px, py, 0, 0, 0, 0, 0)
-    if isinstance(tokens, int):
-        total_tokens += tokens
+    for offset in (0, OFFSET):
+        px = int(prize[1]) + offset
+        py = int(prize[2]) + offset
 
-submit(total_tokens)
+        # original equations
+        # px = ax * n + bx * m
+        # py = ay * n + by * m
+
+        # multiply both equations by button B to eliminate m
+        # px * by = ax * n * by + bx * m * by
+        # py * bx = ay * n * bx + by * m * bx
+
+        pxby = px * by
+        pybx = py * bx
+        axby = ax * by
+        aybx = ay * bx
+        bxby = bx * by
+
+        p = pxby - pybx
+        a = axby - aybx
+
+        # solve for n
+        n = p / a
+        # use n to solve for m
+        m = (px - (ax * n)) / bx
+
+        if n.is_integer() and m.is_integer():
+            tokens = A_BUTTON_PRICE * int(n) + B_BUTTON_PRICE * int(m)
+            if offset == 0:
+                total_tokens += tokens
+            else:
+                total_tokens_with_offset += tokens
+
+print("Part 1:", total_tokens)
+print("Part 2:", total_tokens_with_offset)
