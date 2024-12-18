98fbe8998b5db7332fdae32358645e4dbb582d0a
Derek <sjukledighet@gmail.com>
2024-12-18 00:44:55 -0500

    Day 18 final solution

--- p18.py
+++ p18.py
@@ -1,35 +1,8 @@
 from collections import defaultdict, deque
 
-from aocd import data, submit
+from aocd import data
 
 MAX_COORD = 70
-# MAX_COORD = 6
-
-# data = """5,4
-# 4,2
-# 4,5
-# 3,0
-# 2,1
-# 6,3
-# 2,4
-# 1,5
-# 0,6
-# 3,3
-# 2,6
-# 5,1
-# 1,2
-# 5,5
-# 2,5
-# 6,5
-# 1,4
-# 0,4
-# 6,4
-# 1,1
-# 6,1
-# 1,0
-# 0,5
-# 1,6
-# 2,0"""
 
 
 def bfs(start, end):
@@ -61,13 +34,12 @@ for line in data.splitlines():
     incoming_bytes.append((x, y))
 
 memory_space = defaultdict(lambda: ".")
-for x, y in incoming_bytes:
+for byte, (x, y) in enumerate(incoming_bytes, 1):
     memory_space[(x, y)] = "#"
     steps = bfs((0, 0), (MAX_COORD, MAX_COORD))
     if steps is None:
         break
-# print(steps)
-print(f"{x},{y}")
-submit(f"{x},{y}")
+    elif byte == 1024:
+        print("Part 1:", steps)
 
-
+print(f"Part 2: {x},{y}")
