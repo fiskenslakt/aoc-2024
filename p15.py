1b43189827b188a4c819353eaf95d90036d2ebcc
Derek <sjukledighet@gmail.com>
2024-12-18 19:34:30 -0500

    Day 15 final solution

--- p15.py
+++ p15.py
@@ -3,19 +3,19 @@ from collections import deque
 from aocd import data
 
 
-def move_robot(obj, x, y, i, j):
+def move_boxes(x, y, i, j):
     nx, ny = x + i, y + j
     if warehouse[(nx, ny)] == ".":
+        warehouse[(nx, ny)] = warehouse[(x, y)]
         warehouse[(x, y)] = "."
-        warehouse[(nx, ny)] = obj
         return True
     elif warehouse[(nx, ny)] == "#":
         return False
     elif warehouse[(nx, ny)] == "O":
-        moved = move_robot("O", nx, ny, i, j)
+        moved = move_boxes(nx, ny, i, j)
         if moved:
+            warehouse[(nx, ny)] = warehouse[(x, y)]
             warehouse[(x, y)] = "."
-            warehouse[(nx, ny)] = obj
 
         return moved
 
@@ -83,7 +83,7 @@ x, y = rx, ry
 for move in moves:
     i, j = directions[move]
 
-    moved = move_robot("@", x, y, i, j)
+    moved = move_boxes(x, y, i, j)
     if moved:
         x += i
         y += j
@@ -131,9 +131,3 @@ for y in range(max_y + 1):
             boxes_coords_sum += y * 100 + x
 
 print("Part 2:", boxes_coords_sum)
-
-# display warehouse
-# for y in range(max_y + 1):
-#     for x in range(max_x + 1):
-#         print(wide_warehouse[(x, y)], end="")
-#     print()
